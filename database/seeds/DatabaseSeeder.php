3c5fa77049fc33e05b0f8811e2ec6226d6ee9471
Régis Lutter <rlutter@lemieuxbedard.com>
2016-03-17 16:39:09 -0400

    Fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -99,7 +99,7 @@ class UsersTableSeeder extends Seeder {
     public function run()
     {
         // Check if no user at 1 exists
-        $user = \User::find(1);
+        $user = User::find(1);
         if(!$user){
             // Create user
             $userAdmin = User::create(array(
